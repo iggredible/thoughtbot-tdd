dab4733c82b411f9cebc10af17103b2a7860edfe
Igor Irianto <igor.irianto@gmail.com>
2022-07-18 09:57:32 -0500

    Refactor current_email

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_14_143802) do
+ActiveRecord::Schema.define(version: 2022_07_15_151348) do
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
 end
